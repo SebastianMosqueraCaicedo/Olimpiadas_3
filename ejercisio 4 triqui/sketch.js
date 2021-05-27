bdec894505443a664cfbb775867260c9d0409d1f
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 10:22:46 -0500

    End result

--- ejercisio 4 triqui/sketch.js
+++ ejercisio 4 triqui/sketch.js
@@ -22,12 +22,27 @@ function draw() {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       block[j][i].draw();
-      if (turn < 3 && (block[j][0].state === 1 && block[j][1].state === 1 &&
+      if ((block[j][0].state === 1 && block[j][1].state === 1 &&
           block[j][2].state === 1) || (block[0][i].state === 1 && block[1][i].state === 1 &&
           block[2][i].state === 1) || (block[0][0].state === 1 && block[1][1].state === 1 &&
           block[2][2].state === 1) || (block[2][0].state === 1 && block[1][1].state === 1 &&
-          block[0][2].state === 1)) {
+          block[0][2].state === 1)) { // state comparison for red in horizontal, vertical and diagonal manners
+        textAlign(CENTER);
+        fill(240, 0, 0);
+        textSize(32);
+        text("Red Wins", block[0][1].getx(), 600);
+        turn = 3;
 
+      }
+      if ((block[j][0].state === 2 && block[j][1].state === 2 &&
+          block[j][2].state === 2) || (block[0][i].state === 2 && block[1][i].state === 2 &&
+          block[2][i].state === 2) || (block[0][0].state === 2 && block[1][1].state === 2 &&
+          block[2][2].state === 2) || (block[2][0].state === 2 && block[1][1].state === 2 &&
+          block[0][2].state === 2)) {
+        textAlign(CENTER);
+        textSize(32);
+        fill(0, 0, 240);
+        text("Blue Wins", block[0][1].getx(), 600);
         turn = 3;
 
       }
@@ -37,32 +52,32 @@ function draw() {
 }
 
 function mousePressed() {
+  if (turn < 3) {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (dist(mouseX, mouseY, block[j][i].getx(), block[j][i].gety()) < 50 &&
+          block[j][i].state === 0) {
+          switch (turn) {
+            case 0:
+              turn = 2;
+              block[j][i].state = 1;
+              console.log(block[j][i].state);
+              break;
+            case 1:
+              turn = 2;
+              block[j][i].state = 1;
+              console.log(block[j][i].state);
+              break;
 
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (dist(mouseX, mouseY, block[j][i].getx(), block[j][i].gety()) < 50 &&
-        block[j][i].state === 0) {
-        switch (turn) {
-          case 0:
-            turn = 2;
-            block[j][i].state = 1;
-            console.log(block[j][i].state);
-            break;
-          case 1:
-            turn = 2;
-            block[j][i].state = 1;
-            console.log(block[j][i].state);
-            break;
+            case 2:
+              turn = 1;
+              block[j][i].state = 2;
+              console.log(block[j][i].state);
+              break;
 
-          case 2:
-            turn = 1;
-            block[j][i].state = 2;
-            console.log(block[j][i].state);
-            break;
-
-          default:
-            break;
-
+            default:
+              break;
+          }
         }
       }
     }
