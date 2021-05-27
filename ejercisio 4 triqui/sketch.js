a7abab4dc54f21d98cdef9ff65e8aaa4fbef8740
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 10:15:09 -0500

    Red wins

--- ejercisio 4 triqui/sketch.js
+++ ejercisio 4 triqui/sketch.js
@@ -4,11 +4,11 @@ for (let j = 0; j < 3; j++) {
 }
 let turn = 0; //so that every time a square is changed, the next is the opposite
 
-for (let index = 0; index < 3; index++) {
+for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    block[j][index] = new Cuadro(150 + (150 * index), 150 + (150 * j), 0);
-    console.log(block[index][j]);
-    console.log(index, j);
+    block[j][i] = new Cuadro(150 + (150 * i), 150 + (150 * j), 0);
+    console.log(block[i][j]);
+    console.log(i, j);
   }
 }
 
@@ -18,28 +18,51 @@ function setup() {
 
 function draw() {
   background(220);
-  for (let index = 0; index < 3; index++) {
+
+  for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      block[j][index].draw();
+      block[j][i].draw();
+      if (turn < 3 && (block[j][0].state === 1 && block[j][1].state === 1 &&
+          block[j][2].state === 1) || (block[0][i].state === 1 && block[1][i].state === 1 &&
+          block[2][i].state === 1) || (block[0][0].state === 1 && block[1][1].state === 1 &&
+          block[2][2].state === 1) || (block[2][0].state === 1 && block[1][1].state === 1 &&
+          block[0][2].state === 1)) {
+
+        turn = 3;
+
+      }
     }
   }
-
+  console.log(turn);
 }
 
 function mousePressed() {
-  for (let index = 0; index < 3; index++) {
+
+  for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      if (dist(mouseX, mouseY, block[j][index].getx(), block[j][index].gety()) < 50 
-      && block[j][index].state === 0) {
+      if (dist(mouseX, mouseY, block[j][i].getx(), block[j][i].gety()) < 50 &&
+        block[j][i].state === 0) {
         switch (turn) {
           case 0:
             turn = 2;
-            block[j][index].state = 1;
-            console.log(block[j][index].state);
+            block[j][i].state = 1;
+            console.log(block[j][i].state);
+            break;
+          case 1:
+            turn = 2;
+            block[j][i].state = 1;
+            console.log(block[j][i].state);
+            break;
+
+          case 2:
+            turn = 1;
+            block[j][i].state = 2;
+            console.log(block[j][i].state);
             break;
 
           default:
             break;
+
         }
       }
     }
