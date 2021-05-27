388351e51745ca33af67eed827a173a3b778ad27
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 10:26:51 -0500

    base constructor for Persona

---
+++ ejercisio 1 constructor personas/Persona.js
@@ -0,0 +1,10 @@
+class Persona {
+    constructor(x, y, piel, sombrero, cara, ojos) {
+        this.x = x;
+        this.y = y;
+        this.piel = piel;
+        this.sombrero = sombrero;
+        this.cara = cara;
+        this.ojos = ojos;
+    }
+}
