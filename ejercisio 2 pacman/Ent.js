486c78fe61fc8549505e5a4a02c50fcedaaa66d9
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 20:59:29 -0500

    Father class

---
+++ ejercisio 2 pacman/Ent.js
@@ -0,0 +1,20 @@
+class Ent {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 255;
+        this.g = 255;
+        this.b = 255;
+        this.vel = 0;
+        this.dir = 0;
+    }
+    draw() {
+
+    }
+    getx() {
+        return this.x;
+    }
+    gety() {
+        return this.y;
+    }
+}
