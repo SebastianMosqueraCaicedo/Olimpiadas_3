4bca7b2176c5c13678f5156181622d5333580781
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 20:45:43 -0500

    End result

--- ejercisio 3 herencia carro/sketch.js
+++ ejercisio 3 herencia carro/sketch.js
@@ -1,10 +1,22 @@
-ejemplo = new BusDoble(200, 200);
+  let carro = new Carro(150,100);
+  let carroMoto = new CarroMoto(150,400);
+  let carroTaxi = new CarroTaxi(150,700);
+
+  let bus = new Bus(500,100);
+let busDoble = new BusDoble(500,400);
+let busMio = new BusMio(500,700);
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 800);
 }
 
 function draw() {
   background(220);
-  ejemplo.draw();
+  carro.draw();
+  carroMoto.draw();
+  carroTaxi.draw();
+  bus.draw();
+  busDoble.draw();
+  busMio.draw();
+  carro.draw();
 }
