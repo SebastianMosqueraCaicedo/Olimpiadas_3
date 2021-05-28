6c2aeb9fa57f4bc0b6d0f7a26ebeb3a84f391cb7
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-27 19:28:55 -0500

    Added base variables to father class and fixed pdf

---
+++ ejercisio 3 herencia carro/MedioTransporte.js
@@ -0,0 +1,19 @@
+class MedioTransporte {
+    constructor(x, y, type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+    }
+    draw() {
+        fill(this.r, this.g, this.b);
+        rectMode(CENTER);
+        rect(this.x, this.y, 375, 75);
+        fill(255);
+        textAlign(CENTER);
+        text("Elija entre Carro, CarroMoto, CarroTaxi, Bus, BusMio o BusDoble", this.x, this.y);
+        //giving the chance for user immput
+    }
+}
--- ejercisio 3 herencia carro/sketch.js
+++ ejercisio 3 herencia carro/sketch.js
@@ -1,7 +1,10 @@
+ejemplo = new MedioTransporte(200, 200, null);
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  ejemplo.draw();
 }
