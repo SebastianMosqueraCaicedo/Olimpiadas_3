997a5be5db1906cbc16bef89b51ce96878f14d48
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-28 14:07:45 -0500

    Final version

--- ejercisio 8 brick breaker/Bloque.js
+++ ejercisio 8 brick breaker/Bloque.js
@@ -1,15 +1,20 @@
 class Bloque extends Ent {
-    constructor(x, y) {
+    constructor(x, y, fila) {
         super(x, y);
-        this.fila = 0;
+        this.fila = fila;
         this.r = 50 * this.fila;
         this.g = 255 - (50 * this.fila);
         this.b = 50;
         this.w = 50;
         this.h = 20;
+        this.vivo = true;
     }
     draw() {
         super.params();
-        rect(this.x, this.y, this.w, this.h);
+        if (this.vivo === true) {
+            rect(this.x, this.y, this.w, this.h);
+        } else {
+            this.x = -500;
+        }
     }
 }
--- ejercisio 8 brick breaker/sketch.js
+++ ejercisio 8 brick breaker/sketch.js
@@ -1,16 +1,93 @@
-bloque = new Bloque(50, 50);
+bloque = [];
+for (let i = 0; i < 5; i++) {
+  bloque.push(new Array(4));
+}
 jugador = new Jugador(200, 350);
 bola = new Bola(200, 200);
 
 function setup() {
   createCanvas(400, 400);
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4; j++) {
+      bloque[i][j] = new Bloque(70 + (60 * i), 20 + (25 * j), j);
+    }
+  }
 }
 
 function draw() {
   background(220);
-  bloque.draw();
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4; j++) {
+      bloque[i][j].draw();
+      if (bola.hitUp() > bloque[i][j].hitUp() &&
+        bola.hitUp() < bloque[i][j].hitDo() &&
+        bola.getX() > bloque[i][j].hitLe() &&
+        bola.getX() < bloque[i][j].hitRi()) {
+        bola.vel = -bola.vel;
+        bola.dir = Math.random() * (5) - 2.5;
+        bloque[i][j].vivo = false;
+        console.log(" 4364");
+      }
+
+      if (bola.hitDo() > bloque[i][j].hitUp() &&
+        bola.hitDo() < bloque[i][j].hitDo() &&
+        bola.getX() > bloque[i][j].hitLe() &&
+        bola.getX() < bloque[i][j].hitRi()) {
+        bola.vel = -bola.vel;
+        bola.dir = Math.random() * (5) - 2.5;
+        bloque[i][j].vivo = false;
+        console.log(" 4364");
+      }
+
+      if (bola.hitRi() > bloque[i][j].hitLe() &&
+        bola.hitRi() < bloque[i][j].hitRi() &&
+        bola.getY() > bloque[i][j].hitUp() &&
+        bola.getY() < bloque[i][j].hitDo()) {
+        bola.dir = -bola.dir;
+        bloque[i][j].vivo = false;
+        console.log(" 4364");
+      }
+
+      if (bola.hitLe() > bloque[i][j].hitLe() &&
+        bola.hitLe() < bloque[i][j].hitRi() &&
+        bola.getY() > bloque[i][j].hitUp() &&
+        bola.getY() < bloque[i][j].hitDo()) {
+        bola.dir = -bola.dir;
+        bloque[i][j].vivo = false;
+        console.log(" 4364");
+      }
+
+    }
+  }
+
   jugador.draw();
   bola.draw();
+
+  if (bola.hitUp() > jugador.hitUp() &&
+    bola.hitUp() < jugador.hitDo() &&
+    bola.getX() > jugador.hitLe() &&
+    bola.getX() < jugador.hitRi()) {
+    bola.vel = -bola.vel;
+    bola.dir = Math.random() * (5) - 2.5;
+    console.log(" 4364");
+  }
+
+  if (bola.hitDo() > jugador.hitUp() &&
+    bola.hitDo() < jugador.hitDo() &&
+    bola.getX() > jugador.hitLe() &&
+    bola.getX() < jugador.hitRi()) {
+    bola.vel = -bola.vel;
+    bola.dir = Math.random() * (5) - 2.5;
+    console.log(" 4364");
+  }
+
+  if (bola.hitRi() > jugador.hitLe() &&
+    bola.hitRi() < jugador.hitRi() &&
+    bola.getY() > jugador.hitUp() &&
+    bola.getY() < jugador.hitDo()) {
+    bola.dir = -bola.dir;
+    console.log(" 4364");
+  }
 }
 
 function keyPressed() {
