1e126e3160f27f71f0d068ab7a08955b5d6e29c8
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-28 10:21:49 -0500

    Alien shoots

--- ejercisio 6 space invaders/Alien.js
+++ ejercisio 6 space invaders/Alien.js
@@ -5,12 +5,13 @@ class Alien extends Ent {
         this.hp = hp;
         this.life = hp;
         this.seed = Math.random();
+        this.shot = [];
+        this.bullets = 0;
     }
     draw() {
         this.g = (255 / this.hp) * this.life;
         this.r = 255 - ((255 / this.hp) * this.life);
         super.params();
-        console.log(this.dir);
         if (this.life > 0) {
             fill(this.r, this.g, this.b);
             rect(this.x, this.y, 40, 25);
@@ -36,6 +37,30 @@ class Alien extends Ent {
                 this.y += 15;
                 this.dir = 2;
                 this.vel *= 1.2;
+            }
+
+            if ((frameCount % Math.round((((this.seed + 60) * 2) + (120 * Math.random())))) === 0) { //this allows it to speed up
+                this.bullets++;
+
+            }
+
+
+
+            for (let i = 0; i < this.bullets; i++) {
+                console.log(this.shot.length);
+                if (this.bullets > this.shot.length) {
+                    this.shot.push(new Shot(this.x, this.y + 20));
+
+                    if (this.shot[i].life === 0) {
+                        this.bullets--;
+                        this.shot[i] = this.shot.pop();
+                        this.shot.length = this.bullets;
+                        // i couldnt find how to properly delete bullets
+                    }
+
+                }
+                this.shot[i].draw();
+
             }
         }
     }
